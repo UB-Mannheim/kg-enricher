1dc87991270c4f6bd6a18106ade012358fa672b2
shigapov <57352291+shigapov@users.noreply.github.com>
2023-12-19 15:01:06 +0100

    update to v0.1.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="kg-enricher",
-    version='0.1.1',
+    version='0.1.5',
     author="Renat Shigapov",
     license="MIT",
     description="A Python library for enriching strings, entities and KGs using Wikibase knowledge graphs. "
@@ -17,6 +17,8 @@ setup(
     url="https://github.com/UB-Mannheim/kg-enricher",
     install_requires=['geopandas', 'shapely', 'requests', 'appengine-python-standard'],
     packages=find_packages(),
+    package_data={'enricher': ['84.json', '85.json', '86.json', '87.json', '88.json', '89.json', '90.json', '91.json', '92.json']},
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
